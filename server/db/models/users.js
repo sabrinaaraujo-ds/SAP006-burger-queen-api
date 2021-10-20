02da5881ebeb70ad6a62bc7f191743fd301042be
Sabrina <sabrinaaraujo.ds@gmail.com>
2021-10-20 16:37:24 -0300

    seeders para Users, controller retornando dados do banco

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -14,11 +14,13 @@ module.exports = (sequelize, DataTypes) => {
   Users.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
     role: DataTypes.STRING,
-    restaurant: DataTypes.STRING
+    restaurant: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'Users',
+    tableName: 'Users',
   });
   return Users;
 };
